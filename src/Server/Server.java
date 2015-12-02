2c919e07c0310a4f1d1048d5350c2fc449078677
Edgar Persenda <persenda.edgar@etu.unice.fr>
2015-12-02 09:56:46 +0100

    Correction d'erreur

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -14,28 +14,33 @@ public class Server {
 	public static void main(String[] args) {
 		int portNumber = Integer.parseInt(args[0]);
 		Data data = new Data();
-
-		try (
-		    ServerSocket serverSocket = new ServerSocket(portNumber);
-		    Socket clientSocket = serverSocket.accept();
-		    PrintWriter out =
-		        new PrintWriter(clientSocket.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(clientSocket.getInputStream()));
-		) {
-			String inputLine;
-			CommandsInterpretor interpretor = new CommandsInterpretor(out);
-			boolean fin = true;
-            while (fin && (inputLine = in.readLine()) != null) {
-				String[] input = inputLine.split(";");
-				fin = interpretor.execute(input,data);
-            }
-            System.out.println("CONNECTION CLOSED");
-
-		} catch (IOException e) {
+		ServerSocket serverSocket = null;
+		try {serverSocket = new ServerSocket(portNumber);}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
+		while(true) {
+			try (
+					Socket clientSocket = serverSocket.accept();
+					PrintWriter out =
+							new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(clientSocket.getInputStream()));
+			)
+			{
+				String inputLine;
+				CommandsInterpretor interpretor = new CommandsInterpretor(out);
+				boolean fin = true;
+				while (fin && (inputLine = in.readLine()) != null) {
+					String[] input = inputLine.split(";");
+					fin = interpretor.execute(input, data);
+				}
+				System.out.println("CONNECTION CLOSED");
 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
